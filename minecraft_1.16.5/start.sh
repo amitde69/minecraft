53de1038539a2b3bed5d2e8c66652f57abfb3987
root <root@ip-172-31-47-102.eu-central-1.compute.internal>
2021-02-13 01:52:37 +0000

    late night itamar

--- minecraft_1.16.5/start.sh
+++ minecraft_1.16.5/start.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-yum update && yum install git java-1.8.0-openjdk 
+yum update && yum install -y git java-1.8.0-openjdk 
 echo Starting up server....
 sleep 10
 java -Xmx15360M -Xms15360M -jar server.jar nogui
